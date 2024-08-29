573d4c57592dc4ad002e625a2c0734a436e328ba
santiago villarreal arley <115122095+Villarley@users.noreply.github.com>
2024-08-29 15:02:48 -0600

    camera working

--- camera_detection.py
+++ camera_detection.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+import cv2
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Abre la cámara web (0 es normalmente la cámara por defecto)
+camera = cv2.VideoCapture(0)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # Lee un frame de la cámara
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
