7100494c89010b17c39bbf2bd5b84c20f10c076f
santiago villarreal arley <115122095+Villarley@users.noreply.github.com>
2024-08-30 12:37:49 -0600

    minimal update

--- evacuation_protocol.py
+++ evacuation_protocol.py
@@ -1,19 +1,38 @@
+from jetson_sender import JetsonSender
 from motor_control import MotorController
 
-def evacuationJourney(motor_controller):
+class EvacuationProtocol:
+    def __init__(self):
+        self.jetson_sender = JetsonSender()
+        self.motor_controller = MotorController(left_pin=12, right_pin=16)  # Inicializa el controlador de motores con los pines apropiados
+
+    def execute_protocol(self):
+        print("Ejecutando protocolo de evacuación...")
+        evacuation_instruction = "1"
+        response = self.jetson_sender.send_instruction(evacuation_instruction)
+
+        if response:
+            print(f"Respuesta del ESP32: {response}")
+        else:
+            print("No se recibió respuesta del ESP32.")
     
-    print("Iniciando el viaje de evacuación")
-    motor_controller.move_forward(duration=2)
-    motor_controller.turn_left(duration=1)
-    motor_controller.move_forward(duration=2)
-    motor_controller.turn_right(duration=1)
-    motor_controller.move_backward(duration=2)
-    print("Viaje de evacuación completo")
+    def perform_evacuation_journey(self):
+        print("Iniciando el viaje de evacuación")
+        try:
+            self.motor_controller.move_forward(duration=2)
+            self.motor_controller.turn_left(duration=1)
+            self.motor_controller.move_forward(duration=2)
+            self.motor_controller.turn_right(duration=1)
+            self.motor_controller.move_backward(duration=2)
+            print("Viaje de evacuación completo")
+        finally:
+            self.motor_controller.cleanup()
+
+    def execute(self):
+        self.execute_protocol()
+        self.perform_evacuation_journey()
 
 # Uso de ejemplo
 if __name__ == "__main__":
-    motor_controller = MotorController(left_pin=12, right_pin=16)
-    try:
-        evacuationJourney(motor_controller)
-    finally:
-        motor_controller.cleanup()
+    protocolo = EvacuationProtocol()
+    protocolo.execute()
