0cd6cf5b6a9387126d11ff734dad61f1772b5378
santiago villarreal arley <115122095+Villarley@users.noreply.github.com>
2024-08-30 12:30:03 -0600

    typo fixed

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from motor_control import motor_controller
+from motor_control import MotorController
 from evacuation_protocol import evacuationJourney
 from tcp_listener import TCPListener
 
 def main():
     # Inicializa el controlador de motores (aún no usado en este ejemplo)
-    motor_controller = motor_controller (left_pin=12, right_pin=16)
+    motor_controller = MotorController (left_pin=12, right_pin=16)
 
     # Inicializa el protocolo de evacuación
     protocolo = evacuationJourney()
